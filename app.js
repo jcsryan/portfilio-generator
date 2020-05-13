e8c2fb4746e670bfb967cbfaea9e527d3f336f5e
JCSR <johnconnorsryan@gmail.com>
2020-05-13 15:40:28 -0400

    added inquirer

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src');
+//...
 
+// const pageHTML = generatePage(portfolioData);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
